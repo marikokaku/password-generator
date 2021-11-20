92ad568e69334b9c1b3878eac852cda4e58eb830
Mariko <mkaku98@gmail.com>
2021-11-20 11:59:50 -0800

    variables

--- Develop/script.js
+++ Develop/script.js
@@ -17,6 +17,12 @@ var specialChar = ["!","@","#","$","%","^","&","*","-","+","=",];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Variables 
+var passwordLength = "";
+var passwordSpecialCharacter;
+var passwordNumericCharacter;
+var passwordUpperCase;
+var passwordLowerCase;
 
 // Write password to the #password input
 function writePassword() {
